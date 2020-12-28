f0d551ff9bf8bb758ed6695fd8d06ef659953708
dorabt <dora.github@gmail.com>
2020-12-28 18:07:14 +0900

    google analyticsのコード追加。ヘッダにあった開発中文言を削除。

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="app" class="app">
-    <div class="text-center" style="background:#ff8080">
-      注意：このサイトは現在正式リリースへ向けて最終的な調整作業を行っています(目標は今年(2020)中です)。表示されているデータはまだ正しくない恐れがあります。ご了承下さい。
-    </div>
     <TheHeadingNav />
     <router-view />
     <TheFooter />

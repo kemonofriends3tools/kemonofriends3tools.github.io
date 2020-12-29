18f0a91a60746f4ab8db41050d05ab867f8bfb03
dorabt <dora.github@gmail.com>
2020-12-29 11:44:03 +0900

    javascriptが動作しない環境でもサイト名がタイトルに正しく表示されるよう修正

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,13 @@ module.exports = {
   //publicPath Default: '/'
   // publicPath: './',
 
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'けものフレンズ３ツール',
+    },
+  },
+
   // process.env.NODE_ENVは特に定義していない。.env.development等を作成して定義すれば以下のようにENVでSourceMap有無を分岐させられる。
   // productionSourceMap: process.env.NODE_ENV === 'production' ? false : true,
   productionSourceMap: false,
